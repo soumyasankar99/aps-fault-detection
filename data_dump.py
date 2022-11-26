ca6bae6eedc1aac0d25852ff79411a9e3daa323e
soumya-sankar <soumyasankar99@gmail.com>
2022-11-26 18:42:19 +0530

    this is my 1st committ

--- data_dump.py
+++ data_dump.py
@@ -14,5 +14,15 @@ if __name__=="__main__":
     df = pd.read_csv(DATA_FILE_PATH)
     print(f"Rows and columns:{df.shape}")
 
-    #Convert
-    df    
+     #Convert dataframe to json so that we can dump these record in mongo db
+    df.reset_index(drop=True,inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())
+    print(json_record[0])
+
+    #insert converted json record to mongo db
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
+
+       
+       
+       
